e29307e0708d030edfb02ecb9efaf43292ad6955
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2021-11-04 22:43:11 +0100

    Fix ebextations commands

--- Core/commands/createsu.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from Core.models import User
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        if not User.objects.filter(email="admin@admin.com").exists():
-            User.objects.create_superuser("admin@admin.com", "admin123456", "admin", "admin")
