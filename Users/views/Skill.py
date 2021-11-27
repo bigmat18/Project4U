48a2f661fdd4f09c2fac42ad5acf68337981e824
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2021-11-27 12:06:20 +0100

    Adding skills create endpoint

--- Users/views/Skill.py
+++ Users/views/Skill.py
@@ -5,6 +5,7 @@ from rest_framework_api_key.permissions import HasAPIKey
 from django.conf import settings
 import django_filters as filters
 from django.contrib.postgres.search import TrigramSimilarity
+from rest_access_policy import AccessPolicy
 
 
 class SkillFilter(filters.FilterSet):
@@ -20,10 +21,27 @@ class SkillFilter(filters.FilterSet):
                     similarity__gte=0.01).order_by("-similarity")
             return result
         return queryset
+    
+    
+    
+class SkillAccessPolicy(AccessPolicy):
+    statements = [
+        {
+            "action": ["list"],
+            "principal": "*",
+            "effect": "allow"
+        },
+        {
+            "action": ["create"],
+            "principal": "authenticated",
+            "effect": "allow"
+        }
+    ]
 
 
     
 class SkillListView(generics.ListAPIView,
+                    generics.CreateAPIView,
                     viewsets.GenericViewSet):
     """
     list:
@@ -32,10 +50,15 @@ class SkillListView(generics.ListAPIView,
     Ritorna una lista di tutte le skills.
     E' possibile fare una ricerca aggiungendo " /?name= " alla fine dell'endpoints (dove = inserire la stringa da cercare).
     La ricerca viene effetuata tramite triagrammi, quindi accetta errori di battituira maiuscole/minuscole differenti, caratteri speciali.
+    
+    create:
+    Crea una nuova skill.
+    
+    E' possibile creare una nuova skill, è richiesta solo l'autenticazione.
     """
     serializer_class = SkillSerializer
     queryset = Skill.objects.all()
-    permission_classes = []
+    permission_classes = [SkillAccessPolicy]
     if not settings.DEBUG: permission_classes.append(HasAPIKey)
     
     filterset_class = SkillFilter
