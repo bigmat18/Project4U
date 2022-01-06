5382050ce425b5e280fa8583deed0d3c49c3e090
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-01-06 23:49:01 +0100

    Adding tags to projects tests

--- Projects/tests/Project.py
+++ Projects/tests/Project.py
@@ -2,7 +2,9 @@ from django.http import request, response
 from Core.models import Project, User
 from Core.tests import BaseTestCase
 from rest_framework import status
+from django.test import tag
 
+@tag('Projects')
 class ProjectsTestCase(BaseTestCase):
     
     def setUp(self):
@@ -11,20 +13,23 @@ class ProjectsTestCase(BaseTestCase):
         self.new_user = User.objects.create_user(email=f"2{self.email}", password=self.email,
                                             first_name=self.first_name, last_name=self.last_name)
         
-        
+    @tag('get','auth')  
     def test_projects_list_auth(self):
         response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        
+    
+    @tag('get','unauth')  
     def test_projects_list_unauth(self):
         self.client.logout()
         response = self.client.get('/api/projects/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-        
+    
+    @tag('post','auth')  
     def test_projects_create_auth(self):
         response = self.client.post('/api/projects/', data={"name": "test1"})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     
+    @tag('put','unauth')  
     def test_projects_update_auth(self):
         data = {"name":"new_name"}
         response = self.client.patch(f'/api/projects/{self.project.id}/', data=data)
@@ -35,16 +40,19 @@ class ProjectsTestCase(BaseTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertDictEqual(data, dict(response.json()))
 
+    @tag('put','unauth')  
     def test_projects_update_unauth(self):
         data = {"name":"new_name"}
         self.client.force_authenticate(user=self.new_user)
         response = self.client.patch(f'/api/projects/{self.project.id}/', data=data)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
     
+    @tag('delete','auth')  
     def test_projects_delete_auth(self):
         response = self.client.delete(f'/api/projects/{self.project.id}/')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        
+    
+    @tag('delete','unauth')   
     def test_projects_delete_unauth(self):
         self.client.force_authenticate(user=self.new_user)
         response = self.client.delete(f'/api/projects/{self.project.id}/')
---
+++ Projects/tests/__init__.py
@@ -0,0 +1,3 @@
+from .Project import ProjectsTestCase
+from .Role import RoleTestCase
+from .UserProject import UserProjectTestCase
